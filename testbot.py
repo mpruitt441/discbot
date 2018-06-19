9b6e80197b591e0d796cb7005bee034281ff8007
Mahendra Pruitt <mpruitt441@gmail.com>
2018-06-18 21:07:10 -0400

    Purge function only deleted within 14 days, added a scriptdelete function that deletes a message every 3.5 secconds UNLESS THE BOT IS STOPPED. Future commits will need to have a method to stop the deleting process

--- testbot.py
+++ testbot.py
@@ -28,14 +28,34 @@ async def test(x):
 async def on_message(message):
     if message.content.upper().startswith('!PURGE'):
         x = int(message.content.split()[1])
-        y = message.author.roles
         
-        if "419588713207824394" in [role.id for role in message.author.roles]:
-            print("Got command clear from", message.author)
-            deleted = await bot.purge_from(message.channel, limit =x)
+        if "201405333195849729" in [role.id for role in message.author.roles]:
+            print("Got command purge from", message.author)
+            deleted = await bot.purge_from(message.channel, limit =x, check=None, before=None, after=None, around=None)
             await bot.send_message(message.channel, 'Deleted %s message(s)' % len((deleted)))
-    #if message.content.upper().startswith('!SLAP') slap command
+        else:
+            await bot.send_message(message.channel, "You don't have permission to perform that command!")
+            print("Denied request to purge channel from", message.author)
+            
+    if message.content.upper().startswith('!SCRIPTDELETE'):
+        number = int(message.content.split()[1])
+        if "201405333195849729" in [role.id for role in message.author.roles]:
+            print("Got command scriptdelete from ", message.author, "\nNumber of messages: ", number)
+            number = int(number) 
+            counter = 0
+            async for x in bot.logs_from(message.channel, limit = number):
+                if counter < number:
+                    await bot.delete_message(x)
+                    counter = counter + 1
+                    await asyncio.sleep(3.5)
+            bot.send_message(message.channel, "Completed")
+            print("Completed")
+        else:
+            await bot.send_message(message.channel, "You don't have permission to perform that command!")
     
+                    
+        #if message.content.upper().startswith('!SLAP') slap command
+
 
 ''' purge command?
 @bot.command()
@@ -47,12 +67,9 @@ async def purge(x):
         await bot.say('Deleted %s message(s)' % len((deleted)))
     else:
         bot.say("You do not have permissions to execute that command!")
-
 '''
 
 
     
 
 bot.run("")
-
-
